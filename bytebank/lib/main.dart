79702f9e14f36e105994eb63b0b8405fb18aa838
Diego Lacerda <diego.lacerda.alves@gmail.com>
2023-08-04 09:35:45 -0300

    first commit

---
+++ bytebank/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(title: Text('Transferências'),),
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.add),
+    ),
+  ),
+));
